dae0481b4261f32299d0ed1f6323df803f912122
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2022-07-13 15:24:24 +0630

    Updated a little bit.

---
+++ StateManagements/Riverpod/movie_app/lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+// Flutter: Existing Libraries
+import 'package:flutter/material.dart';
+
+// Flutter: External Libraries
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// HomePage: StatelessWidget Class
+class HomePage extends StatelessWidget {
+  // Static & Constant: Class Properties
+  static const String title = "Home";
+  static const String routeName = "/home";
+
+  // Constructor
+  const HomePage({Key? key}) : super(key: key);
+
+  // build: Override Class Method
+  @override
+  Widget build(BuildContext context) {
+    // Returning Widgets
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(title),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text(title),
+        ),
+      ),
+    );
+  }
+}
